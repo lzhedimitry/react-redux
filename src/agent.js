eb2e3449f8c3dd3399b88381f0d434d6412f4d61
lzhedimitry <40291891+lzhedimitry@users.noreply.github.com>
2022-02-17 16:14:19 +0200

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://nyamtsu.pp.ua/api';
+const API_ROOT = process.env.REACT_APP_BACKEND_URL;
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
